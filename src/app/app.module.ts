bb21655e3dcb19044f8cdcc1a00a4a340d9a205f
Deivide Paulino <deivide.santos@djsystem.com>
2021-03-29 13:15:56 -0300

    esqueleto finalizado
    estrutura montada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './shared/nav/nav.component';
+import { CardComponent } from './shared/card/card.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    CardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

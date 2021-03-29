6a96ecfe35d2cd61bc47ba0259766504c33b9751
Deivide Paulino <deivide.santos@djsystem.com>
2021-03-29 14:41:32 -0300

    rotas criadas
    todas as rotas do menu foram criadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './autenticacao/cadastro/cadastro.component';
+import { LoginComponent } from './autenticacao/login/login.component';
+import { CardComponent } from './shared/card/card.component';
+import { CarrinhoComponent } from './shared/card/carrinho/carrinho.component';
+import { ContatoComponent } from './shared/card/contato/contato.component';
+import { ProdutosComponent } from './shared/card/produtos/produtos.component';
+import { NavComponent } from './shared/nav/nav.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'inicio', component: CardComponent  },
+  { path: 'produtos', component: ProdutosComponent  },
+  { path: 'carrinho', component: CarrinhoComponent  },
+  { path: 'contato', component: ContatoComponent  },
+  { path: 'login', component:   LoginComponent},
+  { path: 'cadastro', component: CadastroComponent  },
+  { path: 'nav', component: NavComponent  },
+  
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './shared/nav/nav.component';
 import { CardComponent } from './shared/card/card.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { ProdutosComponent } from './shared/card/produtos/produtos.component';
+import { ContatoComponent } from './shared/card/contato/contato.component';
+import { CarrinhoComponent } from './shared/card/carrinho/carrinho.component';
+import { LoginComponent } from './autenticacao/login/login.component';
+import { CadastroComponent } from './autenticacao/cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     CardComponent,
-    FooterComponent
+    FooterComponent,
+    ProdutosComponent,
+    ContatoComponent,
+    CarrinhoComponent,
+    LoginComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,

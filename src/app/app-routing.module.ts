4c833c114026ae65759fd93785dbffe183289c1c
Deivide Paulino <deivide.santos@djsystem.com>
2021-03-29 15:31:55 -0300

    rotas
    rotas finalizadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,17 +6,19 @@ import { CardComponent } from './shared/card/card.component';
 import { CarrinhoComponent } from './shared/card/carrinho/carrinho.component';
 import { ContatoComponent } from './shared/card/contato/contato.component';
 import { ProdutosComponent } from './shared/card/produtos/produtos.component';
+import { FooterComponent } from './shared/card/footer/footer.component';
 import { NavComponent } from './shared/nav/nav.component';
 
 const routes: Routes = [
 
+  { path: 'nav', component: NavComponent  },
   { path: 'inicio', component: CardComponent  },
   { path: 'produtos', component: ProdutosComponent  },
   { path: 'carrinho', component: CarrinhoComponent  },
   { path: 'contato', component: ContatoComponent  },
   { path: 'login', component:   LoginComponent},
   { path: 'cadastro', component: CadastroComponent  },
-  { path: 'nav', component: NavComponent  },
+  { path: 'rodape', component: FooterComponent  }
   
   
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,24 +6,24 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './shared/nav/nav.component';
 import { CardComponent } from './shared/card/card.component';
-import { FooterComponent } from './shared/footer/footer.component';
 import { ProdutosComponent } from './shared/card/produtos/produtos.component';
 import { ContatoComponent } from './shared/card/contato/contato.component';
 import { CarrinhoComponent } from './shared/card/carrinho/carrinho.component';
 import { LoginComponent } from './autenticacao/login/login.component';
 import { CadastroComponent } from './autenticacao/cadastro/cadastro.component';
+import { FooterComponent } from './shared/card/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     CardComponent,
-    FooterComponent,
     ProdutosComponent,
     ContatoComponent,
     CarrinhoComponent,
     LoginComponent,
-    CadastroComponent
+    CadastroComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
